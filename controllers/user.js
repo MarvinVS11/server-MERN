36f67053e9fc1b76249ff1a880aa4d3206a6cf05
Marvin <marvi11@hotmail.es>
2023-02-09 13:12:05 -0600

    Actualizacion de manteniemiento de usuarios

--- controllers/user.js
+++ controllers/user.js
@@ -49,9 +49,51 @@ async function createUser(req, res){
    //console.log(req.files.avatar);
    // res.status(200).send({msg:"OK"});
 }
+
+async function updateUser(req, res){
+    const {id} = req.params;
+    const userData = req.body;
+
+    //Pasword
+    if(userData.password){
+        const salt = bcrypt.genSaltSync(10);
+        const hasPassword = bcrypt.hashSync(userData.password, salt);
+        userData.password = hasPassword;
+    }
+    else{
+        delete userData.password;
+    }
+    //Avatar
+    if(req.files.avatar){
+        const imagePath = image.getFilePath(req.files.avatar);
+        userData.avatar= imagePath;
+    }
+
+    User.findByIdAndUpdate({_id: id}, userData, (error)=>{
+        if(error){
+            res.status(400).send({msg:"Error al actualizar el usuario"});
+
+        }else{
+            res.status(200).send({msg:"Actualizacion correcta"});
+        } 
+
+    });
+}
+async function deleteUser (req, res){
+    const {id}= req.params;
+    User.findByIdAndDelete(id,(error)=>{
+        if(error){ 
+            res.status(400).send({msg:"Error al eliminar el usuario"});
+        }else{
+            res.status(200).send({msg:"Usuario eliminado"});
+        }
+    })
+}
 module.exports={
     getMe,
     getUsers,
-    createUser
+    createUser,
+    updateUser,
+    deleteUser
 
 }
--- router/user.js
+++ router/user.js
@@ -9,4 +9,7 @@ const api = express.Router();
 api.get("/user/me", [md_auth.asureAuth],UserController.getMe);
 api.get("/users",[md_auth.asureAuth], UserController.getUsers);
 api.post("/user", [md_auth.asureAuth, md_upload], UserController.createUser);
-module.exports= api; 
+api.patch("/user/:id", [md_auth.asureAuth, md_upload], UserController.updateUser);
+api.delete("/user/:id", [md_auth.asureAuth], UserController.deleteUser);
+
+module.exports= api;  
